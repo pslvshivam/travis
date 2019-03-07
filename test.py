d16505629d8a802d4b8fbdd09a68198da1624974
Shivam CHAUDHARY <47984632+pslvshivam@users.noreply.github.com>
2019-03-07 22:50:48 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
+from calc import add
+from calc import subtract
+from calc import multiply
+from calc import divide
 
+def test_check_add():
+    assert add ( 5 , 2 ) == 7
+    
+def test_check_wrong_add():
+    assert add ( 9 , 4 ) == 7
+
+def test_check_subtract():
+    assert subtract ( 5 , 2 ) == 3
+
+def test_check_wrong_subtract():
+    assert subtract ( 7 , 2 ) == 3
+
+def test_check_multiply():
+    assert multiply ( 5 , 2 ) == 10
+    
+def test_check_divide():
+    assert divide ( 5 , 2 ) == 2.5
